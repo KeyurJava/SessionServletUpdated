c4ad4835113ced9843b0f7e6e2b5241f36ee8bb9
Keyur <keyur.patel0921@gmail.com>
2023-06-29 09:55:27 -0400

    version 2.0

---
+++ src/main/java/demo/ProfileServlet.java
@@ -0,0 +1,29 @@
+package demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ProfileServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		request.getRequestDispatcher("link.jsp").include(request, response);
+
+		HttpSession session = request.getSession(false);
+		String name = (String) session.getAttribute("name");
+		if (session != null && name != null) {
+
+			out.print("Hello, " + name + " Welcome to Profile");
+		} else {
+			out.print("Please login first");
+			request.getRequestDispatcher("login.jsp").include(request, response);
+		}
+		out.close();
+	}
+}
